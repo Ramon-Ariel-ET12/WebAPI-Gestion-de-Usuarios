6f10142b22e3c9860e4581ef5d97a80ff234e15c
Ramon-Ariel-ET12 <ramonarielet12d1@gmail.com>
2024-09-14 02:15:49 -0300

    Create UserApiDBContext.cs
    Se creó para hacer el uso de la migración y así mismo crear la base de datos a través de las clases (Se traduciran a sql)

---
+++ UserApi.Infrastructure/Persistence/UserApiDBContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using UserApi.Domain.Entities;
+
+namespace UserApi.Infrastructure.Persistence;
+
+public class UserApiDBContext : DbContext
+{
+    public UserApiDBContext(DbContextOptions<UserApiDBContext> options) : base(options) { }
+
+    public DbSet<Usuario> Usuario { get; set; }
+    public DbSet<Rol> Rol { get; set; }
+    public DbSet<UsuarioRol> UsuarioRol { get; set; }
+}
