7b52beacb519acdeb364a2858b18ec47d23e3851
Ramon-Ariel-ET12 <ramonarielet12d1@gmail.com>
2024-09-14 02:06:18 -0300

    Create Program.cs
    Se implementó el carter y services
    
    el carter lo usé para que funcione correctamente el swagger.
    el services para que swagger reconozca los endpoints de mis controladores.
    
    También se implementó la conexion a la base de datos (Postgres).

---
+++ UserApi.Presentation/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using UserApi.Infrastructure.Persistence;
+using Carter;
+using UserApi.Infrastructure.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure the conection to PostgreSQL
+builder.Services.AddDbContext<UserApiDBContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ABCDB")));
+
+// Add Carter services
+builder.Services.AddCarter();
+
+// Add Service Manager xd
+builder.Services.AddServiceManager();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// Use Carter
+app.MapCarter();
+
+app.Run();
