e2890efb3991f9e1e9abceda8afa1ad031708107
Ramon-Ariel-ET12 <ramonarielet12d1@gmail.com>
2024-09-14 22:14:49 -0300

    Update UserApiDBContext.cs
    Se agregó estas lineas por si las dudas

--- UserApi.Infrastructure/Persistence/UserApiDBContext.cs
+++ UserApi.Infrastructure/Persistence/UserApiDBContext.cs
@@ -11,3 +11,8 @@ public class UserApiDBContext : DbContext
     public DbSet<Rol> Rol { get; set; }
     public DbSet<UsuarioRol> UsuarioRol { get; set; }
 }
+
+// dotnet ef migrations add FirstMigration --context UserApiDBContext --project .\UserApi.Infrastructure\ --startup-project .\UserApi.Presentation\
+
+
+// dotnet ef database update --context UserApiDBContext --project .\UserApi.Infrastructure\ --startup-project .\UserApi.Presentation\
