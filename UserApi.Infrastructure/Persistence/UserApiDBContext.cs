05048e63c0aa9bb621da82dffd594baa993112f6
Ramon-Ariel-ET12 <Ramonarielet12d1@gmail.com>
2024-09-30 14:43:40 -0300

    Se borró lo innecesario

--- UserApi.Infrastructure/Persistence/UserApiDBContext.cs
+++ UserApi.Infrastructure/Persistence/UserApiDBContext.cs
@@ -11,16 +11,4 @@ public class UserApiDBContext : DbContext
     public DbSet<Rol> Rol { get; set; }
     public DbSet<UsuarioRol> UsuarioRol { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
-            optionsBuilder.UseNpgsql("Host=10.120.1.161;Database=Escuela;Username=administrador;Password=Pass123!");
-        }
-    }
 }
-
-// dotnet ef migrations add FirstMigration --context UserApiDBContext --project .\UserApi.Infrastructure\ --startup-project .\UserApi.Presentation\
-
-
-// dotnet ef database update --context UserApiDBContext --project .\UserApi.Infrastructure\ --startup-project .\UserApi.Presentation\
